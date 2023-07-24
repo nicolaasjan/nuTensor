f5f9ae12be339c5e4e9a7c532d5002d63211ce88
nicolaasjan <nicoenkarin@gmail.com>
2023-07-24 11:49:07 +0200

    Update make-assets.sh

--- tools/make-assets.sh
+++ tools/make-assets.sh
@@ -18,13 +18,9 @@ if [ -n "${TRAVIS_TAG}" ]; then
 fi
 
 mkdir $DES/thirdparties
-cp -R ../nuAssets/thirdparties/hosts-file.net             $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/mirror1.malwaredomains.com $DES/thirdparties/
 cp -R ../nuAssets/thirdparties/pgl.yoyo.org               $DES/thirdparties/
 cp -R ../nuAssets/thirdparties/publicsuffix.org           $DES/thirdparties/
 cp -R ../nuAssets/thirdparties/someonewhocares.org        $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/winhelp2002.mvps.org       $DES/thirdparties/
-cp -R ../nuAssets/thirdparties/www.malwaredomainlist.com  $DES/thirdparties/
 mkdir $DES/umatrix
 cp -R ../nuAssets/recipes/*                               $DES/umatrix/
 
